082bd5c085290dbae37167d13331a3392d7d9081
Samuelfrm <samuelfrm@outlook.com>
2017-10-11 01:35:47 -0500

    Quitamos prompt

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 
 var express = require('express');
 var app = express();
-var Confirm = require('prompt-confirm');
 
 var port=process.env.PORT||3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hola pinche putita');
 });
 
-var confirm = new Confirm('¿Funciono?')
-  .ask(function(answer){
-    console.log(answer);
-  });
-var confirm = new Confirm('¿Funciono?')
-  .run()
-  .then(function(answer) {
-    res.send(answer);
-  });
 
 app.listen(port, function () {
   console.log('Example app listening on port 3000!');
