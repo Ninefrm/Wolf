cc5a0993b49a9636a82d24cba0ecc28a1d079f06
Samuelfrm <samuelfrm@outlook.com>
2017-10-11 01:17:28 -0500

    Agregamos prompt-confirm

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 
 var express = require('express');
 var app = express();
-var Confirm = require('prompt-confirm');
-var prompt = new Confirm('¿Funciono?');
 
 var port=process.env.PORT||3000;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+var confirm = new Confirm('¿Funciono?')
+  .ask(function(answer){
+    console.log(answer);
+  });
 
 app.listen(port, function () {
   console.log('Example app listening on port 3000!');
